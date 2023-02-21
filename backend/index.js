8953d54d0bd2484332313d0f39feaf98f31e7cbb
panosbakk <panosbakaloudis@gmail.com>
2023-02-21 04:15:09 +0200

    today

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express')
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.set("strictQuery", false);
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to the database.");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
+    process.exit();
+  });
+
+  app.listen(8765, () => {
+    console.log("Server is listening on port 9103.");
+  });
